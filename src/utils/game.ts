c8e94a88b722fc2f105270abf57a98d583b0ab4a
stiven trujillo <strujillodev@gmail.com>
2024-09-04 22:17:36 -0500

    fix: getBingoLetter

--- src/utils/game.ts
+++ src/utils/game.ts
@@ -1,7 +1,8 @@
 export function getBingoLetter(number: number): string {
   const letters = ["b", "i", "n", "g", "o"];
   if (number < 1 || number > 75) {
-    return "Número fuera de rango";
+    // Número fuera de rango
+    return "";
   }
   return letters[Math.floor((number - 1) / 15)];
 }
